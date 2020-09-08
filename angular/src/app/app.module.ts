3f2ab83d58b28b209928855f608d690881395710
Foresty <florent.manenc@gmail.com>
2020-09-08 16:23:55 +0200

    Create movielist and moviedetail

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './Movie/movie-list/movie-list.component';
+import { MovieDetailComponent } from './Movie/movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule
